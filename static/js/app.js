eac7e457d3e43925e09693197329f176495e4bd5
Ian Parr <ianjudparr@gmail.com>
2020-02-11 19:40:30 +0000

    Add jQuery document.ready

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,18 @@
-$('.carousel.carousel-slider').carousel({
-  fullWidth: false,
-  indicators: true,
-  duration:200
-});
 
-autoplay();
-function autoplay() {
+$(document).ready(function () {
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: false,
+    indicators: true,
+    duration: 200
+  });
+
+  autoplay();
+  function autoplay() {
     $('.carousel').carousel('next');
     setTimeout(autoplay, 4500);
-}
+  }
 
-var el = document.querySelector('.tabs');
-var instance = M.Tabs.init(el, {});
+  var el = document.querySelector('.tabs');
+  var instance = M.Tabs.init(el, {});
+});
